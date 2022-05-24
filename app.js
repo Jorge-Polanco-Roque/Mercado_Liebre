095976cfa06141c8101f8264f913cd48d15ca170
Jorge Polanco <55784702+Jorge-Polanco-Roque@users.noreply.github.com>
2022-05-23 20:00:42 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Para instalar express, en la consola poner: npm install express
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){console.log(`Servidor corriendo en el puerto ${PORT}`)});
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + '/views/home.html')});
+
+app.get("/login", function(req,res){
+    res.sendFile(__dirname + '/views/login.html')});
+
+app.get("/register", function(req,res){
+    res.sendFile(__dirname + '/views/register.html')});
+    
+        
+
+
