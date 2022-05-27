eeb5f49c2606bef204f99ccd3e4b67fa60acc21c
Jorge Polanco <55784702+Jorge-Polanco-Roque@users.noreply.github.com>
2022-05-27 17:39:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // Para instalar express, en la consola poner: npm install express
 
 const express = require("express");
-const app = express('path);
+const path = require('path');
+const app = express();
 const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
-
-app.listen(PORT, function(){console.log(`Servidor corriendo en el puerto ${PORT}`)});
+app.listen(PORT, ()=>{
+    console.log('Servidor corriendo en el puerto 3000')
+});
 
 app.get("/", function(req,res){
     res.sendFile(path.resolve(__dirname, './view/home.html')}
