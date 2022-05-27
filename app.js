8c63871c7aa8449faf944eb68b2bde1e8090364e
Jorge Polanco <55784702+Jorge-Polanco-Roque@users.noreply.github.com>
2022-05-27 17:28:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 app.listen(PORT, function(){console.log(`Servidor corriendo en el puerto ${PORT}`)});
 
