87d1fa0c00c7bfa6e6051c15b9d1d9ea711e1b7a
Jorge Polanco <55784702+Jorge-Polanco-Roque@users.noreply.github.com>
2022-05-27 17:35:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 // Para instalar express, en la consola poner: npm install express
 
 const express = require("express");
-const app = express();
+const app = express('path);
+const publicPath = path.resolve(__dirname, './public');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
 app.listen(PORT, function(){console.log(`Servidor corriendo en el puerto ${PORT}`)});
 
 app.get("/", function(req,res){
-    res.sendFile(__dirname + '/view/home.html')});
-
-app.get("/login", function(req,res){
-    res.sendFile(__dirname + '/view/login.html')});
+    res.sendFile(path.resolve(__dirname, './view/home.html')}
+                ));
+    
+ app.get("/login", function(req,res){
+    res.sendFile(path.resolve(__dirname, './view/login.html')}
+                ));
 
 app.get("/register", function(req,res){
-    res.sendFile(__dirname + '/view/register.html')});
-
-app.get("/", function(req,res){
-    res.sendFile(__dirname + '/public/style.css')});
-    
+    res.sendFile(path.resolve(__dirname, './view/register.html')}
+                ));
         
 
 
