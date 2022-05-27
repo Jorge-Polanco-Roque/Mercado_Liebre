eb76c578c18346b62b5d84cde5d3d58db6e0ef83
Jorge Polanco <55784702+Jorge-Polanco-Roque@users.noreply.github.com>
2022-05-27 17:10:35 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use(express.static(__dirname + '/public'));
 app.listen(PORT, function(){console.log(`Servidor corriendo en el puerto ${PORT}`)});
 
 app.get("/", function(req,res){
-    res.sendFile(__dirname + '/views/home.html')});
+    res.sendFile(__dirname + '/view/home.html')});
 
 app.get("/login", function(req,res){
-    res.sendFile(__dirname + '/views/login.html')});
+    res.sendFile(__dirname + '/view/login.html')});
 
 app.get("/register", function(req,res){
-    res.sendFile(__dirname + '/views/register.html')});
+    res.sendFile(__dirname + '/view/register.html')});
     
         
 
